f5e48fd4ba6f5a0cb313907ab326da9abec7a6a0
Lavolin <lavolin@gmail.com>
2022-11-05 11:21:00 +0200

    Add project files.

---
+++ APITesting/Controllers/TimeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APITesting.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TimeController : ControllerBase
+    {
+        [HttpGet]
+        [Route("get")]
+        public IActionResult GetCurrentTime()
+        {
+            return Ok(new { currentTime = DateTime.Now.ToLocalTime() });
+        }
+    }
+}
